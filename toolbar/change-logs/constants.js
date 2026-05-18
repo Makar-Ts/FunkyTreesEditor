45cfbefeb5ab88f7a98e4249603d45b31c7527ea
Makar Ts <tsivikov.makar@gmail.com>
2026-05-19 01:27:31 +0300

    0.5.3ver | loading screen and improved changelog (#4)
    * 0.5.3ver | loading screen and improved changelog

--- toolbar/change-logs/constants.js
+++ toolbar/change-logs/constants.js
@@ -12,6 +12,24 @@ export const CHANGE_LOGS_HTML_TEMPLATE = `
 
 export const CHANGE_LOGS = [
   {
+    name: "0.5.3",
+    tags: ["0.5.3"],
+    date: new Date(2026, 5, 19),
+    content: `<p>Added a loading screen and a slightly improved changelog modal.</p>`,
+  },
+  {
+    name: "0.5.2",
+    tags: ["0.5.2"],
+    date: new Date(2026, 5, 18),
+    content: `<p>Added tags for Discord's Embeds, Favicon and aria-labels.</p>`,
+  },
+  {
+    name: "0.5.1",
+    tags: ["0.5.1"],
+    date: new Date(2026, 5, 18),
+    content: `<p>Added Google Analytics.</p>`,
+  },
+  {
     name: "0.5.0",
     tags: ["0.5.0", "0.5.0.b3", "0.5.0.b2", "0.5.0.b1"],
     date: new Date(2026, 5, 18),
--- toolbar/change-logs/main.js
+++ toolbar/change-logs/main.js
@@ -4,6 +4,7 @@ const changeLogs = document.getElementById('change_logs');
 const modal = document.getElementById('change_logs_modal');
 const closeModal = document.getElementById('close_change_logs_modal');
 
+const upperContainer = document.getElementById('change_logs_modal_container');
 const container = document.getElementById('change_logs_modal_logs');
 for (const log of CHANGE_LOGS) {
   let out = CHANGE_LOGS_HTML_TEMPLATE
@@ -16,22 +17,77 @@ for (const log of CHANGE_LOGS) {
   container.innerHTML += out;
 }
 
+let showAmount = 1;
 const show = () => {
   modal.style['display'] = 'block';
 
-  const rect = container.children[0].getBoundingClientRect();
-  container.style['maxHeight'] = `${rect.height}px`;
+  if (showAmount === 1) {
+    requestAnimationFrame(() => {
+      calculateMaxHeight();
+    });
+  } else {
+    requestAnimationFrame(() => {
+      let acc = 0;
+      const gap = 40;
+      for (let index = 0; index < Math.min(showAmount, container.children.length); index++) {
+        const element = container.children[index];
+        
+        acc += element.getBoundingClientRect().height + gap;
+      }
+      container.style['maxHeight'] = `${acc - gap}px`;
+    })
+  }
 }
 
+const calculateMaxHeight = () => {
+  const containerRect = container.getBoundingClientRect();
+  const mid = (containerRect.top + containerRect.bottom) / 2;
+
+  let h = 0;
+  let minDistance = 100_000;
+
+  for (const el of container.children) {
+    const rect = el.getBoundingClientRect();
+
+    const dist = Math.abs(mid - ((rect.bottom + rect.top) / 2));
+    console.log(dist, rect.height);
+    if (dist <= minDistance) {
+      minDistance = dist;
+      h = rect.height;
+    }
+  }
+
+  container.style.maxHeight = `${h}px`; 
+  if (h < 160) {
+    upperContainer.style.padding = `${(160-h) / 2 + 10}px 20px`
+  } else {
+    upperContainer.style.padding = `10px 20px`
+  }
+};
+
+container.addEventListener('scroll', calculateMaxHeight);
 changeLogs.addEventListener('click', show)
 
 closeModal.addEventListener('click', () => {
   modal.style['display'] = 'none';
+  container.style.maxHeight = '40px';
+  showAmount = 1;
 })
 
 
 const lastVievedLog = localStorage.getItem('lastLog');
-if (!lastVievedLog || lastVievedLog != CHANGE_LOGS[0].name) {
+if (!lastVievedLog) {
+  localStorage.setItem('lastLog', CHANGE_LOGS[0].name);
+  showAmount = CHANGE_LOGS.length;
+  show();
+} else if (lastVievedLog != CHANGE_LOGS[0].name) {
+  const index = CHANGE_LOGS.findIndex((v) => v.name == lastVievedLog);
+  if (index === -1) {
+    showAmount = CHANGE_LOGS.length;
+  } else {
+    showAmount = index;
+  }
+
   localStorage.setItem('lastLog', CHANGE_LOGS[0].name);
   show();
 }
